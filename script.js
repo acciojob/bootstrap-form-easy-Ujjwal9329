70ca622dc9a6c22ffa917e5988247ba56082dd85
Ujjwal9329 <ujjwalgujral8@gmail.com>
2025-02-02 13:54:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+document.addEventListener("DOMContentLoaded", function () {
+    let form = document.getElementById("internship-form");
+
+    form.addEventListener("submit", function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+    });
+});
